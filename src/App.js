aa0a85648c906932de59a2165b808e4003098276
Gianluca Di Vita <gianlucajdivita@gmail.com>
2022-12-17 01:59:29 -0500

    Updated front-end
    Current Error: Gas issue with distributeFunds() function

--- src/App.js
+++ src/App.js
@@ -1,167 +1,223 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import { ethers } from "ethers";
+import React from 'react';
+import { ethers } from 'ethers';
 
-// Import the contract artifact
-import MyMusicStreamingService from "contract/Platform.json";
+// Replace with your Goreli testnet provider URL
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// Replace with your contract address
+const contractAddress = '0x1B21D88EDF0Ac2FF1ba6AdDc57C07F49704DF560';
+
+// Replace with your contract ABI
+const contractABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address payable",
+				"name": "artist",
+				"type": "address"
+			}
+		],
+		"name": "addArtist",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "distributeFunds",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "artist",
+				"type": "address"
+			}
+		],
+		"name": "incrementArtistInteraction",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "artistInteractions",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getContractBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalInteractions",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+// Create a contract object
+const contract = new ethers.Contract(contractAddress, contractABI, provider.getSigner());
 
 function App() {
-  // Declare state variables
-  const [provider, setProvider] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [artistAddress, setArtistAddress] = useState("");
-  const [artistInteractions, setArtistInteractions] = useState(null);
-  const [totalInteractions, setTotalInteractions] = useState(null);
-  const [contractBalance, setContractBalance] = useState(null);
-  const [amount, setAmount] = useState("");
+  const [artistAddress, setArtistAddress] = React.useState('');
+  const [artists, setArtists] = React.useState([]);
+  const [depositAmount, setDepositAmount] = React.useState('');
+  const [balance, setBalance] = React.useState(0);
+  const [valueSent, setValueSent] = React.useState(0);
 
-  useEffect(() => {
-    // Connect to the Ethereum provider
-    async function requestAccount() {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      setProvider(provider);
 
-      // Get the contract address from the deployment
-      const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    // Call the addArtist function of the contract
+    const addArtist = async () => {
+      try {
+        await window.ethereum.sendAsync({ method: 'eth_requestAccounts' });
+        await contract.addArtist(artistAddress);
+        const artistInteractions = await contract.artistInteractions(artistAddress);
+        setArtists([...artists, { address: artistAddress, interactions: artistInteractions.toString() }]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-      // Connect to the contract
-      const contract = new ethers.Contract(
-        contractAddress,
-        MyMusicStreamingService.abi,
-        provider.getSigner()
-      );
-      setContract(contract);
 
-      // Get the artist interactions and total interactions from the
-      // Get the artist interactions and total interactions from the contract
-      const [artistInteractions, totalInteractions] = await Promise.all([
-        contract.artistInteractions(),
-        contract.totalInteractions(),
-      ]);
-      setArtistInteractions(artistInteractions);
-      setTotalInteractions(totalInteractions);
-
-      // Get the contract balance from the contract
-      const contractBalance = await contract.getContractBalance();
-      setContractBalance(contractBalance);
+  // Call the incrementInteractions function of the contract
+  const incrementArtistInteraction = async (artistIndex) => {
+    try {
+      await window.ethereum.sendAsync({ method: 'eth_requestAccounts' });
+      const artist = artists[artistIndex];
+      await contract.incrementArtistInteraction(artist.address);
+      const updatedInteractions = await contract.artistInteractions(artist.address);
+      const updatedArtists = [...artists];
+      updatedArtists[artistIndex] = { ...artist, interactions: updatedInteractions.toString() };
+      setArtists(updatedArtists);
+    } catch (error) {
+      console.error(error);
     }
-  }, []);
+  };
 
-  // Function to add an artist to the platform
-  async function handleAddArtist(artistAddress) {
-    // Call the contract's addArtist function
-    await contract.addArtist(artistAddress);
-    setArtistAddress("");
+  // Call the deposit function of the contract
+  const deposit = async () => {
+    try {
+      await window.ethereum.sendAsync({ method: 'eth_requestAccounts' });
+      await contract.deposit({ value: ethers.utils.parseEther(depositAmount) });
+      const updatedBalance = await contract.getContractBalance();
+      setBalance(updatedBalance);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    // Update the artist interactions and total interactions
-    const [artistInteractions, totalInteractions] = await Promise.all([
-      contract.artistInteractions(),
-      contract.totalInteractions(),
-    ]);
-    setArtistInteractions(artistInteractions);
-    setTotalInteractions(totalInteractions);
-  }
 
-  // Function to increment the interaction count for an artist
-  async function handleIncrementArtistInteraction(artistAddress) {
-    // Call the contract's incrementArtistInteraction function
-    await contract.incrementArtistInteraction(artistAddress);
 
-    // Update the artist interactions and total interactions
-    const [artistInteractions, totalInteractions] = await Promise.all([
-      contract.artistInteractions(),
-      contract.totalInteractions(),
-    ]);
-    setArtistInteractions(artistInteractions);
-    setTotalInteractions(totalInteractions);
-  }
+    // Call the distributeFunds function of the contract
+    const distributeFunds = async () => {
+      try {
+        const initialBalance = await contract.getContractBalance();
 
-  // Function to deposit funds to the contract
-  async function handleDeposit(amount) {
-    // Call the contract's deposit function
-    await contract.deposit({ value: amount });
+        const tx = {
+          // other fields of the transaction object
+          gasLimit: 20000000000, // specify the gas limit here
+        };
 
-    // Update the contract balance
-    const contractBalance = await contract.getContractBalance();
-    setContractBalance(contractBalance);
-  }
+        await window.ethereum.sendAsync({ method: 'eth_requestAccounts', tx});
+        await contract.distributeFunds();
+        const updatedBalance = await contract.getContractBalance();
+        const valueSent = initialBalance.sub(updatedBalance);
 
-  // Function to distribute funds to the artists
-  async function handleDistributeFunds() {
-    // Call the contract's distributeFunds function
-    await contract.distributeFunds();
+        setBalance(updatedBalance);
+        setValueSent(valueSent);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    // Update the contract balance
-    const contractBalance = await contract.getContractBalance();
-    setContractBalance(contractBalance);
-  }
+  React.useEffect((artistInfo, setArtistInfo) => {
+    const updateInteractions = async () => {
+      const artistInteractions = await contract.artistInteractions(artistAddress);
+      setArtistInfo({
+        ...artistInfo,
+        interactions: artistInteractions.toString()
+      });
+    };
+    if (artistAddress) {
+      updateInteractions();
+    }
+  }, [artistAddress]);
+
 
   return (
-    <div className="App">
-      <h1>My Music Streaming Service</h1>
-      <div>
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            await handleAddArtist(artistAddress);
-          }}
-        >
-          <label>
-            Add artist:
-            <input
-              type="text"
-              value={artistAddress}
-              onChange={(e) => setArtistAddress(e.target.value)}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-      {artistInteractions
-        ? Object.keys(artistInteractions).map((artist) => (
-            <p key={artist}>
-              <button onClick={() => handleIncrementArtistInteraction(artist)}>
-                Increment Artist Interaction
-              </button>{" "}
-              {artist}: {artistInteractions[artist]}
-            </p>
-          ))
-        : null}
-      {totalInteractions ? (
-        <p>
-          <strong>Total Interactions:</strong> {totalInteractions}
-        </p>
-      ) : null}
-      {contractBalance ? (
-        <p>
-          <strong>Contract Balance:</strong> {contractBalance}
-        </p>
-      ) : null}
-      <div>
-        <form
-          onSubmit={async (e) => {
-            e.preventDefault();
-            await handleDeposit(amount);
-          }}
-        >
-          <label>
-            Deposit:
-            <input
-              type="text"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-            />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-      <p>
-        <button onClick={() => handleDistributeFunds()}>
-          Distribute Funds
-        </button>
-      </p>
+    <div>
+      <input
+        type="text"
+        placeholder="Enter artist address"
+        value={artistAddress}
+        onChange={(event) => setArtistAddress(event.target.value)}
+      />
+      <button onClick={addArtist}>Add artist</button>
+
+      <ul>
+        {artists.map((artist, index) => (
+          <li key={artist.address}>
+            {artist.address}: {artist.interactions}
+            <button onClick={() => incrementArtistInteraction(index)}>Increment interactions</button>
+            <p>Value sent to recipients: {valueSent}</p>
+          </li>
+        ))}
+      </ul>
+
+      <input
+        type="text"
+        placeholder="Enter deposit amount"
+        value={depositAmount}
+        onChange={(event) => setDepositAmount(event.target.value)}
+      />
+      <button onClick={deposit}>Deposit</button>
+
+      <p>Balance: {balance.toString()}</p>
+      <button onClick={distributeFunds}>Distribute funds</button>
     </div>
   );
-}
+ }
+
 
 export default App;
